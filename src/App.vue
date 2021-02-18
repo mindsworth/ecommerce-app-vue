fb56c5becfb5ae0a82ef82f2a49dc01afa3aba6d
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-18 12:24:16 +0100

    setup and add header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header />
   <router-view />
 </template>
 
+<script>
+import Header from "./components/Header";
+
+export default {
+  name: "App",
+  components: { Header }
+};
+</script>
+
 <style lang="scss">
-#app {
+@import "./assets/styles/_variables.scss";
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  font-size: 1vw;
 }
 
 #nav {
---
+++ src/components/Header/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="nav-header">
+    <router-link to="/">
+      <img :src="logoUrl" alt="Logo" class="nav-header__logo" />
+    </router-link>
+    <div class="nav-header__links">
+      <router-link to="/">
+        Home
+      </router-link>
+      <router-link to="/collection">
+        Collection
+      </router-link>
+      <a v-scroll-to="'#section-one'">
+        About
+      </a>
+      <router-link to="/">Login</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { logo } from "../../assets";
+
+export default {
+  name: "Header",
+  setup() {
+    const logoUrl = logo;
+
+    console.log("logoUrl", logoUrl);
+
+    return { logoUrl };
+  }
+};
+</script>
+
+<style lang="scss">
+.nav-header {
+  position: sticky;
+  top: 0;
+  height: 6vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  backdrop-filter: blur(18px);
+  -webkit-backdrop-filter: blur(18px);
+  padding: 0 6%;
+
+  &__logo {
+    width: 10vw;
+  }
+
+  &__links {
+    a {
+      color: #2c3e50;
+
+      &.router-link-exact-active {
+        color: #42b983;
+      }
+    }
+  }
+}
+</style>
