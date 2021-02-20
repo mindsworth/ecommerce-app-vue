621781fd1c71c9745a8a765dd2a30bf42dfab98f
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-20 21:36:02 +0100

    add ethos section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,30 +3,37 @@
     <Hero />
     <div class="space" />
     <TwoColumnSection :label="'about'" :data="aboutSectionData" />
+    <TwoColumnSection :label="'ethos'" :data="ethosSectionData" />
   </div>
 </template>
 
 <script>
 import TwoColumnSection from "../components/TwoColumnSection";
 import Hero from "../components/Hero";
-import { imgAbout1, imgAbout2 } from "../assets/index";
+import { imgAbout1, imgAbout2, imgEthos1, imgEthos2 } from "../assets/index";
 
 export default {
   name: "Home",
   components: { Hero, TwoColumnSection },
   setup() {
-    const bannerImage = imgAbout1;
-    const bannerTopImage = imgAbout2;
     const aboutSectionData = {
       title: "Who We Are",
       subTitle: "<span>True</span> furniture Masters",
       caption:
         "We are a collaboration of emerging and iconic designers and makers. Each object was designed to bring quality and style to your everyday rituals.",
-      bannerImage,
-      bannerTopImage
+      bannerImage: imgAbout1,
+      bannerTopImage: imgAbout2
+    };
+    const ethosSectionData = {
+      title: "Our ethos",
+      subTitle: "<span>Effortlessly</span> Grande Homes",
+      caption:
+        "Making luxurious and truly grande furniture accessible for all people is our daily drive.",
+      bannerImage: imgEthos1,
+      bannerTopImage: imgEthos2
     };
 
-    return { aboutSectionData };
+    return { aboutSectionData, ethosSectionData };
   }
 };
 </script>
