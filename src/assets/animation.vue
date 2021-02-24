d266ce84653a5e03ea2b27a1bf5b608d07f3e3bb
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-24 10:18:21 +0100

    fix product card animation

--- src/assets/animation.vue
+++ src/assets/animation.vue
@@ -1,14 +1,19 @@
 <script>
-import { TimelineMax } from "gsap";
+import $ from "jquery";
+import gsap, { TimelineMax } from "gsap";
 
 export function productCardAnimation() {
   let tl = new TimelineMax();
 
   if (window.innerWidth > 600) {
-    tl.from(".product-card", 0.6, {
-      opacity: 0,
-      y: -80,
-      stagger: 0.4
+    $(".product-card").each((index, el) => {
+      gsap.from(el, {
+        duration: 0.6,
+        opacity: 0,
+        scale: 0,
+        y: -80,
+        stagger: 0.4
+      });
     });
 
     tl.play();
