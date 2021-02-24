3b67460e61f17b309c62096a24e18d809c5b2bc8
Chigoziem Nwaiwu <princegoziem@gmail.com>
2021-02-24 07:16:36 +0100

    fix scrollBehavior issue

--- src/assets/animation.vue
+++ src/assets/animation.vue
@@ -4,11 +4,6 @@ import { TimelineMax } from "gsap";
 export function productCardAnimation() {
   let tl = new TimelineMax();
 
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-
   if (window.innerWidth > 600) {
     tl.from(".product-card", 0.6, {
       opacity: 0,
